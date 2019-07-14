f1c1043d4ac28d8cc328c1e116b24bce0aa1c698
Levi Deurloo <levimbg@gmaill.com>
2019-07-14 15:35:53 +0200

    Updated package

--- src/app.ts
+++ src/app.ts
@@ -11,8 +11,8 @@ export class App {
     config.options.hashChange = false;
 
     config.map([
-      { route: ['', '/'], name: 'dash', moduleId: PLATFORM.moduleName('views/dash/dash'), title: 'Dash board' },
-      { route: 'page', name: 'dapageh', moduleId: PLATFORM.moduleName('views/page/page'), title: 'page 2' }
+      { route: ['', '/'], name: 'dash', moduleId: PLATFORM.moduleName('views/dash/dash'), title: 'Dash board', nav: true },
+      { route: 'page', name: 'dapageh', moduleId: PLATFORM.moduleName('views/page/page'), title: 'page 2', nav: true }
     ]);
 
     this.router = router;
