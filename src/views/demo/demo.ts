8123d034ff28ad7b72eb9db77b1fe7c7f69497e0
Levi Deurloo <levimbg@gmaill.com>
2019-07-16 03:17:40 +0200

    Function for modals fixed, demo page almost done

--- src/views/demo/demo.ts
+++ src/views/demo/demo.ts
@@ -1,3 +1,18 @@
 export class Demo {
 
+    private toggleModal(id: string) {
+        const modal = document.getElementById(id); // Get the modal by ID
+
+        // Check modalclasslist and update when clicked
+        if (modal.classList.contains('active')) modal.classList.remove('active');
+        else modal.classList.add('active');
+    }
+
+    private togglePopOver(id: string) {
+        const modal = document.getElementById(id); // Get the modal by ID
+
+        // Check modalclasslist and update when clicked
+        if (modal.classList.contains('visible')) modal.classList.remove('visible');
+        else modal.classList.add('visible');
+    }
 }
